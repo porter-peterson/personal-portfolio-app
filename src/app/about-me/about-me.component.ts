f252f1b57facdcd5be34896845a6aa16dc39e5d4
Porter Peterson <59668589+porter-peterson@users.noreply.github.com>
2024-09-29 22:49:30 -0600

    Progress on rounding out my about me page. Applied default angular material style to the whole app/background

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [ MatDividerModule, MatButtonModule ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
